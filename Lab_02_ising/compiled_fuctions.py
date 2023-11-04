ce3894a25826fe00034cd403929977d6f8d3383e
HAL <bartosz.biernacki@int.pl>
2023-11-04 17:53:24 +0100

    Zadanie 2 completed

---
+++ Lab_02_ising/compiled_fuctions.py
@@ -0,0 +1,155 @@
+from numba import njit, prange
+import numpy as np
+
+
+@njit("i1[:, :](u2, f8)", parallel=True)
+def make_grid(side_length: int, spin_up_density: float):
+    grid = np.random.rand(int(side_length * side_length))
+    grid = (grid < spin_up_density).astype(np.int8)
+    grid[grid == 0] = -1
+    grid = grid.reshape(side_length, -1)
+    return grid
+
+
+@njit("f8(i1[:, :], f8, f8)",
+      cache=True, fastmath=True, parallel=True)
+def calculate_hamiltonian_explicitly(
+        grid: np.ndarray,
+        J: float,
+        B: float,
+):
+    nrows, ncols = grid.shape
+    hamiltonian = 0.
+
+    # Don't iterate over last row because row+1 is out of range.
+    # Same about columns
+    for row in prange(nrows - 1):
+        for col in range(ncols - 1):
+            me = grid[row, col]
+            up = grid[row - 1, col]
+            down = grid[row + 1, col]
+            left = grid[row, col - 1]
+            right = grid[row, col + 1]
+
+            hamiltonian += -J * (
+                    me * up + me * down + me * left + me * right) - B * me
+
+    # Now iterate over last row and then last column but except for
+    # value in down right corner of an array.
+    row = nrows - 1
+    for col in range(ncols - 1):
+        me = grid[row, col]
+        up = grid[row - 1, col]
+        down = grid[0, col]
+        left = grid[row, col - 1]
+        right = grid[row, col + 1]
+
+        hamiltonian += -J * (
+                me * up + me * down + me * left + me * right) - B * me
+
+    col = ncols - 1
+    for row in range(nrows - 1):
+        me = grid[row, col]
+        up = grid[row - 1, col]
+        down = grid[row + 1, col]
+        left = grid[row, col - 1]
+        right = grid[row, 0]
+
+        hamiltonian += -J * (
+                me * up + me * down + me * left + me * right) - B * me
+
+    # Now value in down right corner of an array.
+    row, col = nrows - 1, ncols - 1
+
+    me = grid[row, col]
+    up = grid[row - 1, col]
+    down = grid[0, col]
+    left = grid[row, col - 1]
+    right = grid[row, 0]
+
+    hamiltonian += -J * (
+            me * up + me * down + me * left + me * right) - B * me
+
+    return hamiltonian / 2
+
+
+@njit("f8(i1[:, :], u2, u2, f8, f8)", cache=True, fastmath=True)
+def hamiltonian_change_after_mikro_step(
+        grid: np.ndarray,
+        row: int,
+        col: int,
+        J: float,
+        B: float,
+):
+    nrows, ncols = grid.shape
+
+    me_idx = (row, col)
+    up_idx = (row - 1, col)
+    left_idx = (row, col - 1)
+
+    if (row < nrows - 1) and (col < ncols - 1):
+        down_idx = (row + 1, col)
+        right_idx = (row, col + 1)
+    elif (row == nrows - 1) and (col < ncols - 1):
+        down_idx = (0, col)
+        right_idx = (row, col + 1)
+    elif (row < nrows - 1) and (col == ncols - 1):
+        down_idx = (row + 1, col)
+        right_idx = (row, 0)
+    else:
+        down_idx = (0, col)
+        right_idx = (row, 0)
+
+    original_spin_value = grid[me_idx]
+
+    old_part = -J * (
+            grid[me_idx] * grid[up_idx] +
+            grid[me_idx] * grid[left_idx] +
+            grid[me_idx] * grid[down_idx] +
+            grid[me_idx] * grid[right_idx]
+    ) - B * grid[me_idx]
+
+    # Spin change
+    grid[me_idx] = ~original_spin_value
+
+    new_part = -J * (
+            grid[me_idx] * grid[up_idx] +
+            grid[me_idx] * grid[left_idx] +
+            grid[me_idx] * grid[down_idx] +
+            grid[me_idx] * grid[right_idx]
+    ) - B * grid[me_idx]
+
+    # Keep original value of spin on grid
+    grid[me_idx] = original_spin_value
+
+    return -old_part + new_part
+
+
+@njit("void(i1[:, :], f8, f8, f8)", cache=True, fastmath=True)
+def mikro_step_MC(
+        grid: np.ndarray,
+        J: float,
+        B: float,
+        beta: float,
+):
+    nrows, ncols = grid.shape
+
+    row = np.random.randint(0, nrows)
+    col = np.random.randint(0, ncols)
+
+    hamiltonian_change = hamiltonian_change_after_mikro_step(
+        grid=grid, row=row, col=col, J=J, B=B,
+    )
+
+    if np.exp(-beta * hamiltonian_change) < np.random.rand():
+        grid[row, col] = ~grid[row, col]
+
+
+@njit("void(i1[:, :], f8, f8, f8)", cache=True, fastmath=True, parallel=True)
+def makro_step_MC(grid, J, B, beta):
+    for _ in prange(grid.size):
+        mikro_step_MC(grid=grid, J=J, B=B, beta=beta)
+
+
+
+
---
+++ Lab_02_ising/zadanie2.py
@@ -0,0 +1,202 @@
+import numpy as np
+import cProfile
+from pstats import SortKey, Stats
+from PIL import Image
+from tqdm import tqdm
+import os
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import matplotlib.pyplot as plt
+
+import compiled_fuctions
+from fancy_colors import print_rainbow_string
+
+
+class Ising:
+    def __init__(
+            self,
+            side_length: int,
+            spin_up_density: float,
+            J: float,
+            B: float,
+            beta: float,
+            n_makro_steps: int,
+            image_prefix: str = None,
+            animation_filename: str = None,
+            magnetization_filename: str = None,
+    ):
+        self.J = J
+        self.B = B
+        self.beta = beta
+        self.n_makro_steps = n_makro_steps
+
+        self.grid = compiled_fuctions.make_grid(
+            side_length=side_length,
+            spin_up_density=spin_up_density,
+        )
+
+        self.grid_history = np.empty(
+            shape=(n_makro_steps, *self.grid.shape),
+            dtype=np.int8,
+        )
+
+        self.images = []
+        self.image_prefix = image_prefix
+        self.animation_filename = animation_filename
+        self.magnetization_filename = magnetization_filename
+
+        self.correct_animation_filename()
+        self.correct_magnetization_filename()
+
+    def correct_animation_filename(self):
+        if self.animation_filename:
+            if os.path.splitext(self.animation_filename)[1] == '':
+                self.animation_filename += '.gif'
+
+    def correct_magnetization_filename(self):
+        if self.magnetization_filename:
+            if os.path.splitext(self.magnetization_filename)[1] == '':
+                self.magnetization_filename += '.png'
+
+    def run_simulation(self):
+        for step in tqdm(
+                iterable=range(self.n_makro_steps),
+                desc='Przeprowadzanie symulacji',
+                ncols=100,
+                mininterval=1./60,
+                maxinterval=1./10,
+        ):
+            self.grid_history[step] = np.copy(self.grid)
+
+            compiled_fuctions.makro_step_MC(
+                grid=self.grid, J=self.J, B=self.B, beta=self.beta
+            )
+
+        self.grid_history[-1] = np.copy(self.grid)
+
+    def make_images(self):
+        for step in tqdm(
+                iterable=range(self.n_makro_steps),
+                desc='Tworzenie obrazków',
+                ncols=100,
+                mininterval=1. / 60,
+                maxinterval=1. / 10,
+        ):
+            grid = (self.grid_history[step] * 255).astype(np.uint8)
+            image = Image.fromarray(grid)
+
+            self.images.append(image)
+
+    def save_images_multi(self):
+
+        def save_image(img, img_prefix, n):
+            img.save(f'{img_prefix}{n}.png')
+
+        with tqdm(
+                total=len(self.images),
+                desc='Zapisywanie obrazków',
+                ncols=100,
+                mininterval=1. / 60,
+                maxinterval=1. / 10,
+        ) as pbar:
+            with ThreadPoolExecutor(
+                    max_workers=len(self.images)//5) as executor:
+                futures = []
+
+                for step, image in enumerate(self.images):
+                    future = executor.submit(
+                        save_image,
+                        img=image,
+                        img_prefix=self.image_prefix,
+                        n=step,
+                    )
+                    futures.append(future)
+
+                for _ in as_completed(futures):
+                    pbar.update(1)
+
+    def save_images(self):
+        if folder := os.path.split(self.image_prefix)[0]:
+            Path(folder).mkdir(parents=True, exist_ok=True)
+
+        for step, image in tqdm(
+                iterable=enumerate(self.images),
+                total=len(self.images),
+                desc='Zapisywanie obrazków',
+                ncols=100,
+                mininterval=1. / 60,
+                maxinterval=1. / 10,
+        ):
+            image.save(f'{self.image_prefix}{step}.png')
+
+    def save_animation(self):
+        if folder := os.path.split(self.animation_filename)[0]:
+            Path(folder).mkdir(parents=True, exist_ok=True)
+
+        print_rainbow_string(
+            'Zapisywanie animacji:  ' +
+            'Nie ma paska postępu ale powinno pójść szybko ' +
+            '\U0001F44D' * 15,
+        )
+
+        self.images[0].save(
+            f'{self.animation_filename}',
+            save_all=True,
+            append_images=self.images[1:],
+            duration=100,
+            loop=0,
+        )
+
+    def save_magnetization(self):
+        print_rainbow_string(
+            'Obliczenie i zapisanie magnetyzacji:  ' +
+            'Też niestety bez paska postępu ' +
+            '\U0001F525' * 15,
+            cmap='rainbow_r',
+        )
+
+        magnetization = self.grid_history.mean(axis=(1, 2))
+
+        plt.plot(magnetization)
+        plt.xlabel('Makrokrok')
+        plt.ylabel('Magnetyzacja')
+        plt.savefig(f'{self.magnetization_filename}', dpi=300)
+
+    def save_simulation(self):
+        if self.image_prefix or self.animation_filename:
+            self.make_images()
+
+        if self.image_prefix:
+            # self.save_images()
+            self.save_images_multi()
+
+        if self.animation_filename:
+            self.save_animation()
+
+        if self.magnetization_filename:
+            self.save_magnetization()
+
+
+if __name__ == '__main__':
+    ising = Ising(
+        side_length=1200,
+        spin_up_density=0.3,
+        J=1, B=1, beta=0.01,
+        n_makro_steps=250,
+        image_prefix='IMG/step',
+        animation_filename='anim',
+        magnetization_filename='magnetyzacja',
+    )
+
+    with cProfile.Profile() as pr:
+        ising.run_simulation()
+        ising.save_simulation()
+        Stats(pr).strip_dirs().sort_stats(SortKey.TIME).print_stats(5)
+
+    # *************************************************
+    # With side_length=1000, n_makro_steps=3:
+    #   pure python  = 122.70s; 1x
+    #   safe numba   =   0.20s; 615x
+    #   unsafe numba =   0.05s; 2450x
+    # *************************************************
+
